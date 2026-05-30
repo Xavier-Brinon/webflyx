66aaa70aaf2e858563947919eb28096f1072b640
Xavier Brinon <xavier@brinon.eu>
2026-05-30 18:25:01 +0100

    feat: print frequency list

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,4 +1,4 @@
-from stats import nbr_words, char_frequency
+from stats import nbr_words, char_frequency, chars_dict_to_sorted_list
 
 def get_book_text(file_path: str) -> str:
     with open(file_path, 'r', encoding="utf-8") as f:
@@ -9,9 +9,10 @@ def main():
     book_path: str = "./books/frankenstein.txt"
     book: str = get_book_text(book_path)
     words: int = nbr_words(book)
-    message: str = f"Found {words} total words"
-    print(message)
+    print(f"Found {words} total words")
     frequency = char_frequency(book)
-    print(frequency)
+    sorted_list = chars_dict_to_sorted_list(frequency)
+    for char_count in sorted_list:
+        print(char_count)
 
 main()
--- bookbot/stats.py
+++ bookbot/stats.py
@@ -1,3 +1,12 @@
+def sort_on(word_count: tuple[str, int]) -> int:
+    return word_count[1]
+
+def chars_dict_to_sorted_list(frequency: dict[str, int]) -> list[tuple[str, int]]:
+    char_counts: list[tuple[str, int]] = []
+    for char in frequency:
+        char_counts.append((char, frequency[char]))
+    return sorted(char_counts, reverse=True, key=sort_on)
+
 def nbr_words(text: str) -> int:
     return len(text.split())
 
