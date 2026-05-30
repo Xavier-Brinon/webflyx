185888c2d8102f8b95e1c097fc5095c093856e15
Xavier Brinon <xavier@brinon.eu>
2026-05-30 18:38:14 +0100

    feat: add arg cli

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,4 +1,5 @@
 from stats import nbr_words, char_frequency, chars_dict_to_sorted_list
+import sys
 
 def get_book_text(file_path: str) -> str:
     with open(file_path, 'r', encoding="utf-8") as f:
@@ -6,7 +7,10 @@ def get_book_text(file_path: str) -> str:
         return file_content
 
 def main():
-    book_path: str = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path: str = sys.argv[1]
     book: str = get_book_text(book_path)
     words: int = nbr_words(book)
     print(f"Found {words} total words")
