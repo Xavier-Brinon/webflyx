1745fbad9d2fa8c24be900e535787882321e34a8
Xavier Brinon <xavier@brinon.eu>
2026-05-30 09:56:40 +0100

    feat: read a file

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(file_path: str) -> str:
+    with open(file_path, 'r', encoding="utf-8") as f:
+        file_content = f.read()
+        return file_content
+
+def main():
+    book_path = "./books/frankenstein.txt"
+    book = get_book_text(book_path)
+    print(book)
+
+main()
