5f4b658e0d0d6fa383909a84b3cf578594dd014c
Xavier Brinon <xavier@brinon.eu>
2026-06-06 17:43:24 +0100

    feat: split asteroids on impact with bullet

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,12 +1,11 @@
 import sys
 import pygame
 from constants     import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger        import log_state
+from logger        import log_state, log_event
 from player        import Player
 from asteroid      import Asteroid
 from asteroidfield import AsteroidField
 from shot          import Shot
-from logger        import log_event
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -51,7 +50,7 @@ def main():
             for shot in shots:
                 if item.collides_with(shot):
                     log_event("asteroid_shot")
-                    item.kill()
+                    item.split()
                     shot.kill()
 
         for item in drawable:
